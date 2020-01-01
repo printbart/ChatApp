0394fc298d6a54ffad15270963ac7da3d7377d6a
printbart <jklee4365@gmail.com>
2020-01-01 17:45:50 -0500

    Bug Fix: User wont be able to see user_name, user_status when they send a request

--- app/src/main/java/com/example/chatapp/RequestsFragment.java
+++ app/src/main/java/com/example/chatapp/RequestsFragment.java
@@ -179,6 +179,68 @@ public class RequestsFragment extends Fragment {
                                     }
                                 });
                             }
+                            else if(type.equals("sent")){
+                                Button request_sent_btn = holder.itemView.findViewById(R.id.request_accept_button);
+                                request_sent_btn.setText("Request Sent");
+
+                                holder.itemView.findViewById(R.id.request_cancel_button).setVisibility(View.INVISIBLE);
+
+                                UsersRef.child(list_user_id).addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.hasChild("image")){
+                                            final  String requestProfileImage = dataSnapshot.child("image").getValue().toString();
+
+                                            Picasso.get().load(requestProfileImage).into(holder.profileImage);
+                                        }
+                                        final  String requestUserName = dataSnapshot.child("name").getValue().toString();
+                                        final  String requestUserStatus = dataSnapshot.child("status").getValue().toString();
+
+                                        holder.userName.setText(requestUserName);
+                                        holder.userStatus.setText("You have sent a request to " + requestUserName);
+
+                                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                CharSequence options[] = new CharSequence[]{
+                                                        "Cancel Chat Request"
+                                                };
+                                                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                                                builder.setTitle("Already sent request");
+
+                                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                                        if (i == 0){
+                                                            ChatRequestRef.child(currentUserID).child(list_user_id).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                    if(task.isSuccessful()){
+                                                                        ChatRequestRef.child(list_user_id).child(currentUserID).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                            @Override
+                                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                                if(task.isSuccessful()){
+                                                                                    Toast.makeText(getContext(), "You have cancelled the chat request", Toast.LENGTH_SHORT).show();
+                                                                                }
+                                                                            }
+                                                                        });
+                                                                    }
+                                                                }
+                                                            });
+                                                        }
+                                                    }
+                                                });
+                                                builder.show();
+                                            }
+                                        });
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
                         }
                     }
 
